c03274eda4b39293133c877951fb7940cffe352e
pedrou103 <pedroluca13.plm40@gmail.com>
2022-11-04 15:51:09 -0300

    inicio do modulo 6

---
+++ database/migrations/2022_11_04_171532_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            // Nome do produto (name) - string
+            $table->string('name');
+            // Código do produto (code) - string - não pode se repetir
+            $table->string('code')->unique();
+            // quantidade do produto (quantity) - string - default 0
+            $table->integer('quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
